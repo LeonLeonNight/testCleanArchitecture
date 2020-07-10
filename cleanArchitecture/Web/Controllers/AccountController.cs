f69e5264499b4de22fb63d2d285c26a7cf86315c
N.Solovyev <N.Solovyev@mininform74.ru>
2020-07-10 14:50:51 +0500

    (#testCleanArchitecture) start refactoring account controller

---
+++ cleanArchitecture/Web/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.ViewModels.Account;
+
+namespace Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController( 
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                var userViewModel = new ApplicationUser
+                {
+                    Email = loginViewModel.Email,
+                    Password = loginViewModel.Password,
+                    
+                };
+            }
+        }
+    }
+}
