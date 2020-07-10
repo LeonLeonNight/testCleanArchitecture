682aebf57aad97655835576441b4ca4ac9a50c70
N.Solovyev <N.Solovyev@mininform74.ru>
2020-07-10 10:48:17 +0500

    (#testCleanArchitect) start initialaize project

---
+++ cleanArchitecture/ApplicationCore/Entities/ContactAggregate/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.Entities.ContactAggregate
+{
+    class Contact : BaseEntity
+    {
+        public string UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LatName { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Email { get; set; }
+    }
+}
